65b9ebb2debb75e43a6196cd6101d1ea0fb4e8cc
Achyutashivam <shivam@achyutagroup.com>
2026-08-10 15:56:20 +0530

    dfgh

--- backend-django/apps/accounts/urls.py
+++ backend-django/apps/accounts/urls.py
@@ -8,7 +8,7 @@ app_name = 'accounts'
 urlpatterns = [
     # Auth URLs
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
+    path('', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
    
 ]
